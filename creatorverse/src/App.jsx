37df85fdce010bf0a20ca43b3ea99e528401682f
taylor <fematr@oregonstate.edu>
2025-06-12 20:50:19 -0700

    Add main and routes

--- creatorverse/src/App.jsx
+++ creatorverse/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useRoutes} from 'react-router-dom';
+import {useState} from 'react';
+import AllCreators from './components/AllCreators.jsx';
+import ViewCreator from './components/ViewCreator.jsx';
+import EditCreator from './components/EditCreator.jsx';
+import AddCreator from './components/AddCreator.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const routes = useRoutes([ {path: '/', element: <AllCreators />}, 
+
+    {path: '/creator/:id', element: <ViewCreator />},
+    {path: '/edit/:id', element: <EditCreator />},
+    {path: '/add', element: <AddCreator />},
+   ])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+       {routes}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
