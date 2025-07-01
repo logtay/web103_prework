5beb2bc07aec3f05c4fe6fb9a639e10cf6aba767
taylor <fematr@oregonstate.edu>
2025-07-01 00:05:29 -0400

    Add gitignore

--- creatorverse/src/client.js
+++ creatorverse/src/client.js
@@ -1,7 +1,8 @@
 import {createClient} from '@supabase/supabase-js';
 
-const URL = 'https://vbjypssfqdzwtnmebkck.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZianlwc3NmcWR6d3RubWVia2NrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NjI4NDUsImV4cCI6MjA2NTMzODg0NX0.ZBMoA5nnnpHknjZEjrMxfghv2CcDiVyPoMkpkcnalZo';
+
+const URL = import.meta.env.VITE_APP_URL;
+const API_KEY = import.meta.env.VITE_APP_KEY;
 
 export const supabase = createClient(URL, API_KEY);
 
